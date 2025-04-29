e92f4c2537ac15a1c80cb234a8667ff0de9d5cbb
RTKartograph <tommasokarolrendo@gmail.com>
2025-04-29 19:43:24 +0200

    Added first stage of user-friendly GUI

--- main.py
+++ main.py
@@ -1,37 +1,70 @@
-from scapy.all import packet, sniff
+from scapy.all import sniff
+from rich.console import Console
+from rich.table import Table
 import sys
 import win32api
+import os
+
+console = Console()
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+       print("Not a windows OS, closing.")
+       sys.exit(1)
+        
 
 class EternalBlue_Counter_Tool:
     def __init__(self):
-        start_inpt = str(input("Make your selection\n1. Packet Detection\n2. EternalBlue Vuln. Detection\nUser Input: "))
+        clear() # Mainly used on 
+        console.print(f"[[bold blue]*[/bold blue]] Eternal[blue]blue[/blue]/Eternal[bright_magenta]romance[/bright_magenta] IDS Tool")
+        console.print("#" * 35, style="bold blue")
+        console.print("Options:", style="bold green")
+        console.print("[bold green]1[/bold green][bold white]. [bold cornflower_blue]Packet Detection[/bold cornflower_blue]")
+        console.print("[bold green]2[/bold green][bold white]. [bold slate_blue3]EternalBlue Vulnerability Detection[/bold slate_blue3]")
+        console.print("#" * 35, style="bold blue")
+
+        start_inpt = console.input("User Input: ")
 
         if start_inpt == "1":
             self.PacketDetect()
         elif start_inpt == "2":
-            self.Check_Vuln()
+            file, version_info = self.srv_locator()
+            self.Check_Vuln(file, version_info)
         else:
             print("Not a correct input type.")
 
     def PacketDetect(self):
-        print("\nStarting SMB sniffing session...")
-        sniff(prn=lambda x: x.summary(), filter="tcp port 445", store=False)
+        console.print("SMB Detection has begun.", style="bold yellow")
+        console.print("(Note: If you don't see anything being logged,\nit means you're receiving no request on [bold pale_turquoise4]PORT 445[/bold pale_turquoise4]\nEffectively, you're safe.)", style="bright_black")
+        filter = "tcp port 445"
+        sniff(prn=self.PacketLogging, filter=filter, store=False)
+    
+    def PacketLogging(self, packet):
+        console.log(f"[bold blue] TCP Packet[/bold blue]: [bold yellow]{packet.summary()}")
+    
+    def srv_locator(self):
+        filepaths = [
+            r"C:\Windows\System32\drivers\srv.sys",
+            r"C:\Windows\System32\drivers\srv2.sys"
+        ]
 
-    def Check_Vuln(self):
-        try:
-            with open(r"C:\Windows\System32\drivers\srv.sys", 'rb') as f:
-                print("srv.sys located. Trying for srv2.sys ...")
-        except FileNotFoundError:
-            try:
-                with open(r"C:\Windows\System32\drivers\srv2.sys", 'rb') as f:
-                    print("srv2.sys located.")
-            except FileNotFoundError:
-                print("Could not find neither, cannot continue.")
-                sys.exit()         
+        for file in filepaths:
+                if os.path.exists(file):
+                    version_info = win32api.GetFileVersionInfo(file, '\\')
+                    return file, version_info
+        console.print("CRITICAL ERROR: Could not find srv.sys or srv2.sys. Stopping the program.", style="bold red")
+        sys.exit(1)
+        
+        
 
-        info = win32api.GetFileVersionInfo(f.name, "\\")
-        ms = info['FileVersionMS']
-        ls = info['FileVersionLS']
+    def Check_Vuln(self, file, version_info):
+        table = Table(title="MS17-010 Diagnosis")
+        table.add_column("Specification", style="red")
+        table.add_column("Result", style="cyan")         
+        ms = version_info['FileVersionMS']
+        ls = version_info['FileVersionLS']
         version = (
             (ms >> 16) & 0xffff,
             ms & 0xffff,
@@ -39,29 +72,33 @@ class EternalBlue_Counter_Tool:
             ls & 0xffff
         )
         humanReadableVersion = '{}.{}.{}.{}'.format(*version)
-        print(f"Current Version: {humanReadableVersion}")
+        # print(f"Current Version: {humanReadableVersion}")
 
         win_ver = sys.getwindowsversion()
-        print(f"Current OS: {win_ver.major}, Minor {win_ver.minor}, Build {win_ver.build}")
+        # print(f"Current OS: {win_ver.major}, Minor {win_ver.minor}, Build {win_ver.build}")
+        table.add_row("Driver Name:", os.path.basename(file))
+        table.add_row("Driver Version:", humanReadableVersion)
+        table.add_row("OS Version:", f"{win_ver.major}.{win_ver.minor}, Build {win_ver.build}")
 
         safe_versions = {
-            (6, 1): (7601, 23689), # Windows 7
             (6, 0): (6002, 19743), # Windows Vista / Windows Server 2008
+            (6, 1): (7601, 23689), # Windows 7
             (6, 3): (9600, 18604), # Windows 8.1 / Windows Server 2012 R2
-            (10, 0): (10240, 17443), # Windows 10
+            (10, 0): (10240, 17443), # Windows 10 and 11
         }
 
         os_key = (win_ver.major, win_ver.minor)
 
         if os_key not in safe_versions:
-            print("OS not supported.")
+            console.print("OS not supported.", style="bright_black") # Why the FUCK is grey called bright black???
         
         safe_build = safe_versions[os_key]
 
+        console.print(table)
         if version[2] < safe_build[0] or (version[2] == safe_build[0] and version[3] < safe_build[1]):
-            print("System is VULNERABLE to MS17-010.")
+            console.print("System is VULNERABLE to MS17-010.", style="bold red")
         else:
-            print("System is PATCHED against MS17-010.")
+            console.print("System is PATCHED against MS17-010.", style="bold green")
         
 
 if __name__ == '__main__':
