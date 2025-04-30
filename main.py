729341c7f3c132e14038529d73b99feeb97c7c31
RTKartograph <tommasokarolrendo@gmail.com>
2025-04-30 11:53:35 +0200

    Added first security measure (port blocking)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scapy.all import sniff
 from rich.console import Console
 from rich.table import Table
+import subprocess
 import sys
 import win32api
 import os
@@ -12,12 +13,14 @@ def clear():
         os.system('cls')
     else:
        print("Not a windows OS, closing.")
-       sys.exit(1)
+       sys.exit()
         
 
 class EternalBlue_Counter_Tool:
     def __init__(self):
-        clear() # Mainly used on 
+        clear() # Mainly used on debug
+        self.RULE_NAME = "Block SMB Port 445"
+
         console.print(f"[[bold blue]*[/bold blue]] Eternal[blue]blue[/blue]/Eternal[bright_magenta]romance[/bright_magenta] IDS Tool")
         console.print("#" * 35, style="bold blue")
         console.print("Options:", style="bold green")
@@ -55,9 +58,7 @@ class EternalBlue_Counter_Tool:
                     version_info = win32api.GetFileVersionInfo(file, '\\')
                     return file, version_info
         console.print("CRITICAL ERROR: Could not find srv.sys or srv2.sys. Stopping the program.", style="bold red")
-        sys.exit(1)
-        
-        
+        sys.exit()    
 
     def Check_Vuln(self, file, version_info):
         table = Table(title="MS17-010 Diagnosis")
@@ -85,6 +86,7 @@ class EternalBlue_Counter_Tool:
             (6, 1): (7601, 23689), # Windows 7
             (6, 3): (9600, 18604), # Windows 8.1 / Windows Server 2012 R2
             (10, 0): (10240, 17443), # Windows 10 and 11
+            # (10, 0): (99999, 99999) # Debugging
         }
 
         os_key = (win_ver.major, win_ver.minor)
@@ -96,10 +98,62 @@ class EternalBlue_Counter_Tool:
 
         console.print(table)
         if version[2] < safe_build[0] or (version[2] == safe_build[0] and version[3] < safe_build[1]):
-            console.print("System is VULNERABLE to MS17-010.", style="bold red")
+            console.print("System is [bold red]VULNERABLE[/bold red] to MS17-010.")
+            self.SafetyPrompt()
         else:
-            console.print("System is PATCHED against MS17-010.", style="bold green")
-        
+            console.print("System is [bold green]PATCHED[/bold green] against MS17-010.")
+    
+    def SafetyPrompt(self):
+        console.print("=" * 30, style="purple")
+        console.print("Options to counteract MS17-010:")
+        console.print("[bold green]1[/bold green]. Make a firewall rule [bold yellow](RECOMMENDED)[/bold yellow]")
+        console.print("[bold green]2[/bold green]. Install the patch [bold red](RISKY)[/bold red]")
+        console.print("[bold green]3[/bold green]. Exit [bold red](NOT RECOMMENDED)[/bold red]")
+        snd_inp = console.input("[underline]User Input:[/underline] ")
+
+        if snd_inp == "1":
+
+            if self.CheckRule():
+                console.print(f"Firewall rule already exists: {self.RULE_NAME}!", style="underline cyan")
+                a = console.input("[yellow]Do you want to remove it? (y/n): [/yellow]").strip().lower()
+                if a == "y":
+                    self.DeleteRule()
+                else:
+                    console.print("[[blue]*[/blue]] Keeping existing firewall rule.")
+            else:
+                self.CreateRule()
+
+        if snd_inp == "2":
+            console.print("This feature is work-in-progress! While you wait, close your port!", style="red")
+        else:
+            print("Exiting the program...")
+            sys.exit()
+    
+    def CheckRule(self):
+        console.print(f"[[blue]*[/blue]] [yellow]Checking if rule exists...[/yellow]")
+        check_rule = subprocess.run(args=['netsh', 'advfirewall', 'firewall', 'show', 'rule', f'name="{self.RULE_NAME}"'],
+                                    capture_output=True, text=True)
+        return "No rule exists with specified criteria" not in check_rule.stdout
+
+    def CreateRule(self):
+        console.print(f"[[blue]*[/blue]] [yellow]Creating firewall rule...[/yellow]")
+        create_rule = subprocess.run(args=['netsh', 'advfirewall', 'firewall', 'add', 'rule', f'name="{self.RULE_NAME}"', 'dir=in', 'action=block', 'protocol=TCP', 'localport=445'],
+                                     capture_output=True, text=True)
+        if create_rule.returncode == 0 and self.CheckRule():
+            console.print(f"[[green]+[/green]] [green]Rule successfully created![/green]")
+        else:
+            console.print(f"[[bold red]*[/bold red]] [bold red]CRITICAL ERROR:[/bold red] Could not create firewall rule!\nMake sure you're running as administrator!")
+            console.print(create_rule.stderr)
+
+    def DeleteRule(self):
+        console.print(f"[[blue]*[/blue]] [yellow]Removing firewall rule...[/yellow]")
+        remove_rule = subprocess.run(args=['netsh', 'advfirewall', 'firewall', 'delete', 'rule', f'name="{self.RULE_NAME}"'],
+                                    capture_output=True, text=True)
+        if remove_rule.returncode == 0:
+            console.print(f"[[red]-[/red]] [red]Rule successfully removed![/red]")
+        else:
+            console.print(f"[[bold red]*[/bold red]] [bold red]CRITICAL ERROR:[/bold red] Could not remove firewall rule!\nMake sure you're running as administrator!")
+            console.print(remove_rule.stderr)
 
 if __name__ == '__main__':
     EternalBlue_Counter_Tool()
